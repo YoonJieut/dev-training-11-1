0b4d607ff9346018b5cf7fc4fd7b7337e959acdf
YoonJunHyeon <qmdldos@gmail.com>
2023-10-19 21:36:28 +0900

    mail 로직, locateCompare 메소드 잘못 사용, type 재설정 및 if 조건문 수정

--- model/mailCheck.js
+++ model/mailCheck.js
@@ -6,31 +6,31 @@
  */
 
 const mailCheck = (value)=>{
+  let result = false;
+  
   if(typeof(value)==="string"){
     // 초기값들
     let valueArr = [...value].map(x => x.charCodeAt());
     let atSignUni = "@".charCodeAt();
     let dotUni = ".".charCodeAt();
     let newArr = [];
-    let result = false;
 
-    console.log(typeof(atSignUni),typeof(valueArr[0]));
 
     // 배열을 순회하며 atSign을 나누고, 쪼개며 다른배열을 배출
     for (let i =0; i<valueArr.length; i++){
-      if(valueArr[i].localeCompare(atSignUni)===0){
-        return newArr= valueArr.slice(i+1);
+      if(valueArr[i] === atSignUni){
+        newArr= valueArr.slice(i+1);
+        break;
       }
     }
     // 나머지 배열을 다시 검사
     newArr.map(x=> {
-          if(x.localeCompare(dotUni)===0 ){
-          return result=true;
-          }
-        }
-      )
-    return result;
+      if(x === dotUni ){
+      return result = true;
+      }
+    });
   }
+  return result;
 };
 
 module.exports = mailCheck;
