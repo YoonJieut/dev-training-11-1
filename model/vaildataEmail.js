00c3d68daf9d024a2ef0c21a52b40213fd6e7c2e
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 19:49:29 +0900

    문자열을 배열로 변환 from 메서드의 등장

--- model/vaildataEmail.js
+++ model/vaildataEmail.js
@@ -1,6 +1,11 @@
 function validateEmail(email){
   if(typeof(email)==="string"){
-
+    // 문자열을 배열로 변환
+    const emailArray = Array.from(email);
+    console.log(email);
+    
+    
+    
   } else {
     console.log(email + "<- 올바른 데이터 타입은 아님")
   }
