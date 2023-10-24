e2ba0943f5d5829973a4e07bacc665ac88ac5c92
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 20:03:38 +0900

    절차형 함수 모듈 작성완료

--- model/vEmail.js
+++ model/vEmail.js
@@ -4,7 +4,21 @@ function validateEmail(email) {
   let hasDot = false;
 
   if(typeof(email) === "string"){
-    
+    // 아래에 반복문에 length 기준의 loop를 구성하기 때문에
+    // length가 부여되는 문자열만 동작할 수 있도록 조건식을 제작함
+
+    for( let i = 0; i < email.length; i ++) {
+      let char = email[i];
+      if(char === "@") {
+        hasAtSign = true;
+      }
+      if (char === '.'){
+        hasDot = true;
+      }
+    }
+
+    return hasAtSign && hasDot;
+
   } else {
     console.log( email )
   }
