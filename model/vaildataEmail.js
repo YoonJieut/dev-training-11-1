9686278314b66100022ba9e71f29baa464dffced
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 19:53:13 +0900

    필터링 메서트 filter의 등장, 배열에서 특정 조건을 만족하는 요소만 추출

--- model/vaildataEmail.js
+++ model/vaildataEmail.js
@@ -4,10 +4,21 @@ function validateEmail(email){
     const emailArray = Array.from(email);
     console.log(email);
     
-    
+    // '@'과 '.' 문자를 필터링
+    const filterArray = emailArray.filter(char => {
+      if(char === '@'){
+        return true;
+      } else if (char === '.'){
+        return true;
+      } else {
+        return false;
+      }
+    });
     
   } else {
     console.log(email + "<- 올바른 데이터 타입은 아님")
   }
 }
+
+validateEmail('Yoonjieut@gmail.com');
 module.exports = validateEmail;
